6a8ffc8117f869fca43b8bd22316b30c7760a897
Aleksandr Alekseev <alekseev.a@atbgroup.ru>
2026-01-29 16:39:50 +0300

    add BaseAvatar component with TypeScript support

---
+++ src/shared/ui/base-avatar/BaseAvatar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img :src="src" class="avatar" alt="Avatar" />
+    <div class="avatar-border"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface BaseAvatarProps {
+  src: string;
+  size: 'default' | 'sm' | 'lg';
+  isActive?: boolean;
+}
+
+withDefaults(defineProps<BaseAvatarProps>(), {
+  size: 'default',
+});
+</script>
+
+<style scoped></style>
